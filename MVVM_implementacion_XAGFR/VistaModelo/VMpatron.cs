9e40b7c272917c2ce72dd96bbc59c37b7c118854
Aimararivv <xitziafierro@gmail.com>
2024-10-16 22:20:51 -0700

    MVVM

---
+++ MVVM_implementacion_XAGFR/VistaModelo/VMpatron.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_implementacion_XAGFR.VistaModelo
+{
+    class VMpatron : BaseViewModel
+    {
+        #region VARIABLES
+        string _Texto;
+        #endregion
+        #region CONTRUCTOR
+
+        #endregion
+        #region OBJETOS
+        public string Texto
+        {
+            get { return _Texto; }
+            set { SetValue(ref _Texto, value); }
+        }
+        #endregion
+        #region PROCESOS
+        public async Task ProcesoAsyncrona()
+        {
+
+        }
+        public void ProcesoSimple()
+        {
+
+        }
+        #endregion
+    }
+}
